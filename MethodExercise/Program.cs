7501e29a94a0ac133b9b89b8a3c36fd100fc3541
tuckerward20 <tuckerward20@gmail.com>
2021-01-22 12:00:23 -0600

    sending ex 1 to git

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void int Add(int num1, int num2)
+        {
+            int num = num1 + num2;
+        }
+        static void int Multiply(int num1, int num2)
+        {
+            int num = num1 * num2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome, tell me you name?:");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?:");
+            var color = Console.ReadLine();
+            Console.WriteLine("What animal do you like best?:");
+            var animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?:");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"So your name is {name} and your favorite color is {color}. +" +
+                $"Strangely, you like this animal: {animal}. That's all great, but {band} sucks.");
+
+        }
+    }
+}
