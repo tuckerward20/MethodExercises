934153952a134d851c46eef74b12c15e42b70611
tuckerward20 <tuckerward20@gmail.com>
2021-01-22 13:17:30 -0600

    completed exercise 2

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -4,18 +4,29 @@ namespace MethodExercise
 {
     class Program
     {
-        static void int Add(int num1, int num2)
+        static void myAdd(params int[] list)
         {
-            int num = num1 + num2;
+            int sum = 0;
+            for (int i = 0; i <=(list.Length-1); i++)
+            {
+                sum = sum + list[i];
+            }
+            Console.WriteLine(sum);
+            
         }
-        static void int Multiply(int num1, int num2)
+        static void myMultiply(params int[] list)
         {
-            int num = num1 * num2;
+            int sum = 1;
+            for (int i = 0; i <= (list.Length - 1); i++)
+            {
+                sum = sum * list[i];
+            }
+            Console.WriteLine(sum);
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome, tell me you name?:");
+            /*Console.WriteLine("Welcome, tell me you name?:");
             var name = Console.ReadLine();
             Console.WriteLine("What is your favorite color?:");
             var color = Console.ReadLine();
@@ -26,7 +37,10 @@ namespace MethodExercise
 
             Console.WriteLine($"So your name is {name} and your favorite color is {color}. +" +
                 $"Strangely, you like this animal: {animal}. That's all great, but {band} sucks.");
+            */
 
+            myAdd(1,2,3,4,5,6);
+            myMultiply(2, 4, 6);
         }
     }
 }
